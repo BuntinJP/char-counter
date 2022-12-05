24cf48cb9dc6c98aa20e9cefb207543589f845e0
Buntin-Synthia <aokitakumi72@gmail.com>
2022-12-05 12:31:19 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Head } from './components/Head';
+import { Counter } from './components/Counter';
+
+function App() {
+    return (
+        <div className="App">
+            <Head />
+            <Counter />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Head.jsx
@@ -0,0 +1,5 @@
+import '../App.css';
+
+export function Head() {
+    return <h1>文字数カウント</h1>;
+}
